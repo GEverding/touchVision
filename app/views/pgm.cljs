3db45ea3f43fde4f9eaae07074725ea9625a1481
Garrett Everding <garrett.everding92@gmail.com>
2014-07-13 10:09:31 -0400

    Refactored PGM view to get d3 brushes working
    - moved all x, y, brush, {x,y} axis into the state
    - added IDidMount interface to render brushes after the DOM is ready

--- app/views/pgm.cljs
+++ app/views/pgm.cljs
@@ -55,39 +55,53 @@
 
 (defn graph [app owner opts]
   (reify
-    om/IRender
-    (render [_]
+    om/IInitState
+    (init-state [_]
       (let [data (:data app)
             width (:width opts)
             height (:height opts)
-            height2 (:height2 opts)
-            margin (:margin opts)
-            margin2 (:margin2 opts)
             x (-> d3 .-time (.scale) (.range [0 width]))
-            ;; x2 = d3.time.scale().range([0, width]),
-            x2 (-> d3 .-time (.scale) (.range [0 width]))
-            ;; y = d3.scale.linear().range([height, 0]),
-            y (-> d3 .-scale (.linear) (.range [height 0]))
-            ;; y2 = d3.scale.linear().range([height2, 0]);
-            y2 (-> d3 .-scale (.linear) (.range [height2 0]))
-            ;; xAxis = d3.svg.axis().scale(x).orient("bottom"),
-            x-axis (-> d3 .-svg (.axis) (.scale x) (.orient "bottom"))
-            ;; xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
-            x-axis2 (-> d3 .-svg (.axis) (.scale x2) (.orient "bottom"))
-            ;; yAxis = d3.svg.axis().scale(y).orient("left");
-            y-axis (-> d3 .-svg (.axis) (.scale y) (.orient "left")) ]
-        (-> x (.domain (-> d3
-                           (.extent
-                             (map
-                               (fn [{:keys [timestamp]}] timestamp)
-                               data)))))
+            y (-> d3 .-scale (.linear) (.range [height 0])) ]
+        {:width width
+         :height height
+         :margin (:margin opts)
+         :x x :y y
+         :brush (-> d3 .-svg (.brush) (.x x))
+         :x-axis (-> d3 .-svg (.axis) (.scale x) (.orient "bottom"))
+         :y-axis (-> d3 .-svg (.axis) (.scale y) (.orient "left"))}))
+    om/IDidMount
+    (did-mount [this]
+      (let [brushg (om/get-node owner "brush")
+            height (om/get-state owner :height)
+            brush (om/get-state owner :brush)]
+        (.log js/console brushg)
+        (.log js/console brush)
+        (-> d3
+            (.select brushg)
+            (.call brush)
+            (.selectAll "rect")
+            (.attr "y" -6)
+            (.attr "height" (+ height 7)))))
+    om/IRenderState
+    (render-state [_ {:keys [x y data width height]}]
+      (let [data (:data app)]
+        (-> x (.domain
+                (-> d3
+                    (.extent
+                      (map
+                        (fn [{:keys [timestamp]}] timestamp)
+                        data)))))
         (-> y (.domain [0 1]))
-        (html [:svg {:height height :width width}
-               (for [d data]
-                 [:rect {:color "red"
-                         :height height
-                         :width 2
-                         :x (x (:timestamp d))}])])))))
+        (html [:svg {:height height
+                     :width width
+                     :ref "svg"}
+               [:g {:class "data"}
+                (for [d data]
+                  [:rect {:color "red"
+                          :height height
+                          :width 2
+                          :x (x (:timestamp d))}])]
+               [:g {:class "brush" :ref "brush"}]]) ) )))
 
 
 (defn pgm-view [app owner]
@@ -102,9 +116,9 @@
             ]
         ;; (om/transact! app :bands (fn [_] data))
         (html [:div {:class "pgm-container js-graph-view"}
-                 (om/build graph app {:opts {:width width :height height
-                                             :margin margin :margin2 margin2
-                                             :height2 height2}})
+               (om/build graph app {:opts {:width width :height height
+                                           :margin margin :margin2 margin2
+                                           :height2 height2}})
 
                ])))))
 
