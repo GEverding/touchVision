23d357953c7eae549bf84a343dc40d0aa7e323c7
Garrett Everding <garrett.everding92@gmail.com>
2014-12-16 21:00:53 -0800

    ocd: removing whitespace

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -2,30 +2,24 @@
 -- Creates a New Recording record
 insert into recording (patient_id) values (:patient_id)
 
-
 -- name: start-recording!
 -- Start Recording
 update recording set start_time = CURRENT_TIMESTAMP where id = :id
-
 
 -- name: stop-recording!
 -- Stop Recording
 update recording set stop_time = CURRENT_TIMESTAMP where id = :id
 
-
 -- name: create-patient<!
 -- Create new Patient Record
 insert into patient (name) values (:name)
 
-
 -- name: append<!
 -- Append new captured data from capture device
 insert into captured_data (recording_id, pressure, x, y, z, timestamp) values(:recording_id, :pressure, :x, :y, :z, :timestamp)
-
 
 -- name: fetch-data-by-record-id
 -- Fetch All rows of a recording
 select id, pressure, x, y, z
 from captured_data
 where recording_id = :recording_id
-
